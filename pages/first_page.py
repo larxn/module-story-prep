c4e801eaf06b85d4290ff0619a7dd71b189f8cb7
Luis Velásquez <luvejo.dev@gmail.com>
2024-01-13 11:29:41 -0500

    chore: use custom css to allow full width iframe

--- pages/first_page.py
+++ pages/first_page.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# embed streamlit docs in a streamlit app
-components.iframe("https://storyprep.vercel.app/", height=862)
+# Custom CSS to make iframe responsive.
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# Application.
+components.iframe("https://storyprep.vercel.app/", height=862) 
