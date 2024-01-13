6f682890232e94628f566ea6be8b4cf1a9e8076c
Luis Velásquez <luvejo.dev@gmail.com>
2024-01-13 16:03:15 -0500

    chore: pass PAT through URL

--- pages/first_page.py
+++ pages/first_page.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import os
 
 # Custom CSS to make iframe responsive.
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Application.
-components.iframe("https://storyprep.vercel.app/", scrolling=True) 
+api_key = os.environ['CLARIFAI_PAT'] 
+components.iframe("https://storyprep.vercel.app/?api_key={api_key}", scrolling=True)
